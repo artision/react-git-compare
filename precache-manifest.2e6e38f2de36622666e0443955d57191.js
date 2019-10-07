f4c3eba9a1c120db0d9efc3f1430b0b34f0ddc52
artision <art.2413@hotmail.com>
2019-10-07 21:19:39 +0200

    Updates

---
+++ precache-manifest.2e6e38f2de36622666e0443955d57191.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-git-compare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-git-compare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-git-compare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-git-compare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-git-compare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "cfbb55d33f262c7868a9",
+    "url": "/react-git-compare/static/js/runtime~main.98aca901.js"
+  },
+  {
+    "revision": "ea492615bd4c036f3d85",
+    "url": "/react-git-compare/static/js/main.e53b5acf.chunk.js"
+  },
+  {
+    "revision": "8ab6aa3fc3c95f38fe74",
+    "url": "/react-git-compare/static/js/2.dfc4ffa2.chunk.js"
+  },
+  {
+    "revision": "8ab6aa3fc3c95f38fe74",
+    "url": "/react-git-compare/static/css/2.e2e9fc0e.chunk.css"
+  },
+  {
+    "revision": "32a3b6bc62604149ae1ab6340c630c6f",
+    "url": "/react-git-compare/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d836bb87978f8571619a73b8bbea965a.js"
+  "/react-git-compare/precache-manifest.2e6e38f2de36622666e0443955d57191.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-git-compare/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
